21a3ef8466c63550862c867434d03159d877ff3e
Fer Uría <fauria@gmail.com>
2016-02-17 21:04:30 +0100

    Rename lap to lamp

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-# Sample index.php file for fauria/lap Docker image.
+# Sample index.php file for fauria/lamp Docker image.
 # To use your own website instead, please refer to the documentation:
-# https://registry.hub.docker.com/u/fauria/lap/
+# https://registry.hub.docker.com/u/fauria/lamp/
 
 phpinfo();
 
