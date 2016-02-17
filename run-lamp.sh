0bcde3d2168330cb18816b519587a333e31550b8
Fer Uría <fauria@gmail.com>
2016-02-17 21:03:27 +0100

    Rename run-lap.sh to run-lamp.sh

---
+++ run-lamp.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+function exportBoolean {
+    if [ "${!1}" = "**Boolean**" ]; then
+            export ${1}=''
+    else 
+            export ${1}='Yes.'
+    fi
+}
+
+exportBoolean LOG_STDOUT
+exportBoolean LOG_STDERR
+
+if [ $LOG_STDERR ]; then
+    /bin/ln -sf /dev/stderr /var/log/apache2/error.log
+else
+	LOG_STDERR='No.'
+fi
+
+if [ $ALLOW_OVERRIDE == 'All' ]; then
+    /bin/sed -i 's/AllowOverride\ None/AllowOverride\ All/g' /etc/httpd/conf/httpd.conf
+fi
+
+if [ $LOG_LEVEL != 'warn' ]; then
+    /bin/sed -i "s/LogLevel\ warn/LogLevel\ ${LOG_LEVEL}/g" /etc/httpd/conf/httpd.conf
+fi
+
+# stdout server info:
+if [ ! $LOG_STDOUT ]; then
+cat << EOB
+    
+    **********************************************
+    *                                            *
+    *    Docker image: fauria/lamp               *
+    *    https://github.com/fauria/docker-lamp   *
+    *                                            *
+    **********************************************
+
+    SERVER SETTINGS
+    ---------------
+    · Redirect Apache access_log to STDOUT [LOG_STDOUT]: No.
+    · Redirect Apache error_log to STDERR [LOG_STDERR]: $LOG_STDERR
+    · Log Level [LOG_LEVEL]: $LOG_LEVEL
+    · Allow override [ALLOW_OVERRIDE]: $ALLOW_OVERRIDE
+    · PHP date timezone [DATE_TIMEZONE]: $DATE_TIMEZONE
+
+EOB
+else
+    /bin/ln -sf /dev/stdout /var/log/apache2/access.log
+fi
+
+# Set PHP timezone
+/bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
+
+# Run Postfix
+/usr/sbin/postfix start
+
+# Run MariaDB
+/usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&
+
+# Run Apache:
+if [ $LOG_LEVEL == 'debug' ]; then
+    /usr/sbin/apachectl -DFOREGROUND -k start -e debug
+else
+    &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
+fi
