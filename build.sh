91b4c925e968b98d4eb3a2ec5380ab1163dbcb51
Elias Alves <unixelias@gmail.com>
2017-11-09 00:56:38 -0200

    Fix 1

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 shopt -s extglob
 
 for i in $( ls docker); do
-      *-test )  ;;
+  case "$i" in
+    *-test )  ;;
       * ) docker build -t unixelias/lamp:$i \
-          --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-          --build-arg VCS_REF=`git rev-parse --short HEAD` docker/lamp-$i \
-          && docker push unixelias/lamp:$i ;;
-  fi
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` docker/$i \
+        && docker push unixelias/lamp:$i ;;
+  esac
 done
-docker tag unixelias/lamp:16.10 unixelias/lamp:latest && docker push unixelias/lamp:latest
+#docker tag unixelias/lamp:16.04 unixelias/lamp:latest && docker push unixelias/lamp:latest
